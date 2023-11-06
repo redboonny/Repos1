e5bb1f885b28baf9a475c95292de3486264ec3fc
redboonny <babitskaia03@gmail.com>
2023-11-06 10:37:30 +0300

    Описание

---
+++ main1.py
@@ -0,0 +1,15 @@
+word = "Some"  # String
+number = 7  # Integer
+num_2 = 5.68132  # Float
+bool = False  # Boolean
+
+word = "Результат:"
+
+number -= 5
+num_3 = 8
+res = number % num_3
+
+num_a = num_z = num_q = 10
+num_4, word_2, bool_2 = 12, "Word", True
+
+print(word, res, num_4, word_2, bool_2)
